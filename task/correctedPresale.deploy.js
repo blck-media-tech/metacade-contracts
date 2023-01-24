262f58208717740ebafbe8708d9a661408c4bb68
Episqol <khomchykdmitry@gmail.com>
2023-01-24 15:51:05 +0300

    ALT-48 add automatic contract writing to contractsData

--- task/correctedPresale.deploy.js
+++ task/correctedPresale.deploy.js
@@ -1,8 +1,12 @@
 const { task } = require("hardhat/config");
 const CONFIG = require("./arguments.deploy.js");
 const delay = require("../helpers/helpers");
+const contractsData = require("./contractsData.json");
+const fs = require("fs");
+const path = require("path");
 
 task("deploy:CorrectedPresale", "Deploys corrected metacade presale contract").setAction(async (taskArgs, hre) => {
+    let networkContractsData = contractsData[hre.network.name];
     const { CorrectedPresale: MetacadePresaleArguments } = CONFIG[hre.network.name];
 
     await hre.run("clean&compile");
@@ -12,6 +16,10 @@ task("deploy:CorrectedPresale", "Deploys corrected metacade presale contract").s
         arguments: MetacadePresaleArguments,
         contract: "MetacadePresale",
     });
+
+    networkContractsData.correctedPresale = MetacadePresale.address;
+
+    fs.writeFileSync(path.resolve(__dirname) + "/contractsData.json", JSON.stringify(contractsData, null, 2));
 
     await delay(60000);
 
--- task/testTasks/betaPresale.deploy.js
+++ task/testTasks/betaPresale.deploy.js
@@ -1,17 +1,26 @@
 const { task } = require("hardhat/config");
 const CONFIG = require("../arguments.deploy.js");
 const delay = require("../../helpers/helpers");
+const contractsData = require("../contractsData.json");
+const fs = require("fs");
+const path = require("path");
+const data = require("../contractsData.json");
 
 task("deploy:BetaPresale", "Deploys beta metacade presale contract").setAction(async (taskArgs, hre) => {
+    let networkContractsData = contractsData[hre.network.name];
     const { MetacadePresale: OriginalPresaleArguments } = CONFIG[hre.network.name];
 
     await hre.run("clean&compile");
-    console.log(OriginalPresaleArguments);
+
     const OriginalPresale = await hre.run("deployment", {
         network: hre.network.name,
         arguments: OriginalPresaleArguments,
         contract: "MetacadeBeta",
     });
+
+    networkContractsData.betaPresale = OriginalPresale.address;
+
+    fs.writeFileSync(path.resolve(__dirname) + "/contractsData.json", JSON.stringify(contractsData, null, 2));
 
     await delay(60000);
 
